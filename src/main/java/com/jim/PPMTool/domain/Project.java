b06676dbd896ee9c27bba4cc69380b587ffbfe64
unknown <jim25522532@gmail.com>
2020-05-22 13:01:48 +0800

    新增Project Model驗證

--- src/main/java/com/jim/PPMTool/domain/Project.java
+++ src/main/java/com/jim/PPMTool/domain/Project.java
@@ -2,12 +2,17 @@ package com.jim.PPMTool.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -15,13 +20,24 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank(message = "Project name is required")
 	private String projectNane;
+	
+	@NotBlank(message = "projectIdentifier is required")
+	@Size(min=4 , max=5 , message="please use 4 to 5 characters")
+	@Column(updatable =false,unique =true)
 	private String projectIdentifier;
+	
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
 	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	public Project() {
